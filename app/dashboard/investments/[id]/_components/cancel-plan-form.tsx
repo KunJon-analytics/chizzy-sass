0ede6ef5872f86e14cddbf53d818c5848885a277
KunJon-analytics <johnadelakun1@gmail.com>
2024-12-07 00:46:24 +0100

    fixed cancel plan modal

--- app/dashboard/investments/[id]/_components/cancel-plan-form.tsx
+++ app/dashboard/investments/[id]/_components/cancel-plan-form.tsx
@@ -1,18 +1,28 @@
 "use client";
 
-import { useTransition } from "react";
-import { useRouter } from "next/navigation";
 import { toast } from "sonner";
+import { useState, useTransition } from "react";
 
 import { Button } from "@/components/ui/button";
+import {
+  Credenza,
+  CredenzaBody,
+  CredenzaClose,
+  CredenzaContent,
+  CredenzaDescription,
+  CredenzaFooter,
+  CredenzaTrigger,
+  CredenzaHeader,
+  CredenzaTitle,
+} from "@/components/ui/credenza";
 import { cancelInvestment } from "@/actions/investments/cancel-investment";
 import { LoadingAnimation } from "@/components/loading-animation";
 
 type CancelPlanFormProps = { investmentId: string };
 
 const CancelPlanForm = ({ investmentId: id }: CancelPlanFormProps) => {
+  const [open, setOpen] = useState(false);
   const [isPending, startTransition] = useTransition();
-  const router = useRouter();
 
   const onSubmit = () => {
     startTransition(async () => {
@@ -27,7 +37,6 @@ const CancelPlanForm = ({ investmentId: id }: CancelPlanFormProps) => {
           if (success) {
             toast.success(`Investment was successfully cancelled`);
           }
-          router.refresh();
         });
       } catch (error) {
         console.error(error);
@@ -35,15 +44,33 @@ const CancelPlanForm = ({ investmentId: id }: CancelPlanFormProps) => {
       }
     });
   };
+
   return (
-    <Button
-      variant="destructive"
-      type="submit"
-      onClick={onSubmit}
-      disabled={isPending}
-    >
-      {isPending ? <LoadingAnimation /> : "Cancel Plan"}
-    </Button>
+    <Credenza open={open} onOpenChange={setOpen}>
+      <CredenzaTrigger asChild>
+        <Button variant="destructive">Cancel Plan</Button>
+      </CredenzaTrigger>
+      <CredenzaContent className="sm:max-w-[425px]">
+        <CredenzaHeader>
+          <CredenzaTitle>Confirm Cancellation</CredenzaTitle>
+          <CredenzaDescription>
+            Warning: This action cannot be undone. If you cancel now, you'll
+            lose all progress on this investment plan. Are you sure you want to
+            proceed?
+          </CredenzaDescription>
+        </CredenzaHeader>
+        <CredenzaBody>
+          <Button variant="destructive" onClick={onSubmit} disabled={isPending}>
+            {isPending ? <LoadingAnimation /> : "Cancel Plan"}
+          </Button>
+        </CredenzaBody>
+        <CredenzaFooter>
+          <CredenzaClose asChild>
+            <button>Close</button>
+          </CredenzaClose>
+        </CredenzaFooter>
+      </CredenzaContent>
+    </Credenza>
   );
 };
 
