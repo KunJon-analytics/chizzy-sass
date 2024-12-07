a5aeee49a412bf66b22824554c543821effde7f6
KunJon-analytics <johnadelakun1@gmail.com>
2024-12-07 17:06:15 +0100

    fixed plans section

--- components/home/pricing.tsx
+++ components/home/pricing.tsx
@@ -2,68 +2,31 @@ import { CircleCheck } from "lucide-react";
 import Balancer from "react-wrap-balancer";
 import Link from "next/link";
 
+import prisma from "@/lib/prisma";
+import { Tranche } from "@prisma/client";
+import { formatCurrency } from "@/lib/utils";
+
 import { Container, Section } from "../ui/craft";
 import { Button } from "../ui/button";
 import { Badge } from "../ui/badge";
 
-interface PricingCardProps {
-  title: "Basic" | "Standard" | "Pro";
-  price: string;
-  description?: string;
-  features: string[];
-  cta: string;
-  href: string;
-}
+const Pricing = async () => {
+  const tranches = await prisma.tranche.findMany({
+    take: 3,
+    orderBy: { fee: "asc" },
+    skip: 1,
+  });
 
-// Dummy pricing data
-const pricingData: PricingCardProps[] = [
-  {
-    title: "Basic",
-    price: "$29/month",
-    description: "Perfect for small businesses and individuals.",
-    features: ["3 Pages", "Basic SEO", "Email Support", "Responsive Design"],
-    cta: "Choose Basic",
-    href: "https://stripe.com/",
-  },
-  {
-    title: "Standard",
-    price: "$59/month",
-    description: "Best for growing businesses with more needs.",
-    features: [
-      "10 Pages",
-      "Advanced SEO",
-      "CMS Integration",
-      "24/7 Chat Support",
-    ],
-    cta: "Choose Standard",
-    href: "https://stripe.com/",
-  },
-  {
-    title: "Pro",
-    price: "$99/month",
-    description: "Ideal for larger businesses that need scalability.",
-    features: [
-      "Unlimited Pages",
-      "E-commerce Integration",
-      "Priority Support",
-      "Custom API Integration",
-    ],
-    cta: "Choose Pro",
-    href: "https://stripe.com/",
-  },
-];
-
-const Pricing = () => {
   return (
     <Section id="tranches">
       <Container className="flex flex-col items-center gap-4 text-center">
-        <h2 className="!my-0 text-3xl">Our Investment Plans</h2>
+        <h2 className="!my-0 text-3xl">Choose Your Investment Plan</h2>
         <p className="text-lg opacity-70 md:text-2xl">
-          <Balancer>The plans we offer is specifically made for you.</Balancer>
+          <Balancer>Tailored investment options for your goals.</Balancer>
         </p>
 
         <div className="not-prose mt-4 grid grid-cols-1 gap-6 min-[850px]:grid-cols-3">
-          {pricingData.map((plan, index) => (
+          {tranches.map((plan, index) => (
             <PricingCard plan={plan} key={index} />
           ))}
         </div>
@@ -72,32 +35,42 @@ const Pricing = () => {
   );
 };
 
-const PricingCard = ({ plan }: { plan: PricingCardProps }) => {
+const PricingCard = ({ plan }: { plan: Tranche }) => {
+  const description = `Stake: ${formatCurrency(
+    plan.fee
+  )}, to start earning in ${plan.cooldownInterval} days.`;
+
   return (
     <div className="flex flex-col rounded-lg border p-6">
       <div className="text-center">
-        <Badge>{plan.title}</Badge>
-        <h4 className="mb-2 mt-4 text-2xl text-primary">{plan.price}</h4>
-        <p className="text-sm opacity-70">{plan.description}</p>
+        <Badge>{plan.name}</Badge>
+        <h4 className="mb-2 mt-4 text-2xl text-primary">
+          {formatCurrency(plan.fee)}
+        </h4>
+        <p className="text-sm opacity-70">{description}</p>
       </div>
 
       <div className="my-4 border-t"></div>
 
       <ul className="space-y-3 text-left">
-        {plan.features.map((feature, i) => (
-          <li key={i} className="flex items-center text-sm opacity-70">
-            <CircleCheck className="mr-2 h-4 w-4" />
-            {feature}
-          </li>
-        ))}
+        <li className="flex items-center text-sm opacity-70">
+          <CircleCheck className="mr-2 h-4 w-4" />
+          Stake: {formatCurrency(plan.fee)}
+        </li>
+        <li className="flex items-center text-sm opacity-70">
+          <CircleCheck className="mr-2 h-4 w-4" />
+          Cool Down: {plan.cooldownInterval} days
+        </li>
+        <li className="flex items-center text-sm opacity-70">
+          <CircleCheck className="mr-2 h-4 w-4" />
+          Daily Profit Increase: {plan.dailyProfitIncrease}%
+        </li>
       </ul>
 
       <div className="mt-auto pt-6">
-        <Link href={plan.href} target="_blank">
-          <Button size={"sm"} className="w-full">
-            {plan.cta}
-          </Button>
-        </Link>
+        <Button size={"sm"} className="w-full" asChild>
+          <Link href={"/dashboard/plans"}>Start earning</Link>
+        </Button>
       </div>
     </div>
   );
